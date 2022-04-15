a7dc6491669ed4ad56f15243af46b3049d174d5e
Lucas Vallejos <lucascolegioripari@gmail.com>
2022-04-15 07:26:35 -0300

    I wrote the functions to separate the left and right operands using array.push and join() to convert it to a string, then i used the Number() method to change it from str to number type, now i got to refactor the operate() function to accept complex mathematical expressions since it only works with two operands

--- script.js
+++ script.js
@@ -1,25 +1,65 @@
-const display = document.querySelector('.display')
+const display = document.querySelector('.display');
+  
+const numberButons = document.querySelectorAll('.keys');
+    numberButons.forEach(el => el.addEventListener('click', addLeftOperand));
+const clearButton = document.querySelector('.keys-clear');
+    clearButton.addEventListener('click',clearEverything);
+const operators = document.querySelectorAll('.keys-operator');
+    operators.forEach(el => el.addEventListener('click', addOperator));
+    
+const equalButton = document.querySelector('.keys-equal');
+    equalButton.addEventListener('click',operate)
 
-const numberButons = document.querySelectorAll('.keys')
-numberButons.forEach(el => el.addEventListener('click', showDisplay))
 
-function add(a,b) {
-    let result = a+b;	
+
+  function add(a,b) {
+     result = (Number(a))+ (Number(b));	
     return result
   };
   
+  let leftOperand = [];
+  let operationToDo = '';
+  let rightOperand = [];
   
-  function operate(numLeft,operator,numRight){
-      let operation = operator ;
-      if(operation == '+'){
-          let result = add(numLeft,numRight)
-          console.log(result)
-          return result
-      }
-      else return 'error'
-      
-  }operate()
 
-  function showDisplay(e){
-      display.textContent = display.textContent + e.target.textContent
+  function operate(a,b,c){
+        if(b == '+'){
+           result = add((a),(c))
+          display.textContent = result;
+          return result
+  }operate(leftOperand.join(''),operationToDo,rightOperand.join(''))
+      
+      
   }
+
+  function addLeftOperand(e){
+     leftOperand.push(Number(e.target.textContent))
+     display.textContent = leftOperand.join('')
+     
+  }
+  function addRightOperand(e){
+      rightOperand.push(Number(e.target.textContent))
+      display.textContent = rightOperand.join('');
+  }
+  
+  
+  function clearEverything(){
+      display.textContent = '';
+      leftOperand = [];
+      operationToDo = '';
+      rightOperand = [];
+}
+  function addOperator(e){
+       numberButons.forEach(el => el.removeEventListener('click', addLeftOperand))
+       numberButons.forEach(el => el.addEventListener('click', addRightOperand))
+       operators.forEach(el => el.addEventListener('click', updateLeftOperand))
+      operationToDo = e.target.textContent;
+      display.textContent = leftOperand.join('') + operationToDo
+}
+function updateLeftOperand(e){
+    leftOperand.push(operationToDo)
+    rightOperand.forEach(el => leftOperand.push(Number(el)))
+    rightOperand = [];
+    display.textContent = rightOperand.join('');
+}
+
